afd2cfc793e5c88fc6639bc42948a6388219639c
PK1308 <princevkurien@gmail.com>
2022-07-27 23:35:42 +0400

    model_trainer done

---
+++ app.py
@@ -0,0 +1,48 @@
+from cmath import log
+from json import load
+import re
+from flask import Flask, request , render_template , jsonify
+import sys
+import os
+import pandas as pd
+from sklearn import preprocessing 
+from IMDB.app_util.util import load_object, save_object
+from IMDB.app_exception.exception import App_Exception
+from nltk.tokenize.toktok import ToktokTokenizer
+import logging
+from bs4 import BeautifulSoup
+import re 
+import nltk 
+nltk.download('stopwords')
+
+
+
+TRAINED_MODEL_PATH = 'production_model/model.pkl'
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    try:
+        if request.method == 'POST':
+            review = request.form['review']
+            review_dict = {'review': review}
+            X = pd.DataFrame(review_dict , index=[0])
+            model =load_object(file_path=TRAINED_MODEL_PATH )
+            transformed_feature = model.preprocessing_object.transform(X)
+            predicted = model.trained_model_object.predict(transformed_feature)
+            prediction = model.label_binarizer_object.inverse_transform(predicted).tolist()[0]
+            return jsonify({"Prediction" : prediction})
+        else:
+            return render_template('index.html')
+    except Exception as e:
+        return str(e)
+
+
+
+
+if __name__ == "__main__":
+    app.run()
